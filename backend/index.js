1e34321f0a1ce43212ab79ce495550bea343ec85
dnlMatos <danilomatosdev@gmail.com>
2025-04-30 21:04:25 -0300

    i

---
+++ backend/index.js
@@ -0,0 +1,35 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import morgan from "morgan";
+import helmet from "helmet";
+
+dotenv.config();
+
+const app = express();
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: process.env.FRONTEND_URL,
+  })
+);
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(morgan());
+app.use(
+  helmet({
+    crossOriginResourcePolicy: false,
+  })
+);
+
+const PORT = 8080 || process.env.PORT;
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
